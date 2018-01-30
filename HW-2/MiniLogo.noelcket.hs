e8b5a973c4d4e0ce53fa34840db5b9aa30d3518e
Thomas <noelcket@oregonstate.edu>
2018-01-29 22:59:10 -0800

    worked on HW2

--- HW-2/MiniLogo.noelcket.hs
+++ HW-2/MiniLogo.noelcket.hs
@@ -1,5 +1,6 @@
 module MiniLogo where
 
+import Prelude hiding (Num)
 --
 -- * MiniLog
 -- Sutdents in Group:
@@ -15,7 +16,53 @@ module MiniLogo where
 --you should use built-in types for num, var, and macro. (if you want to
 --define a type Num you will ahve vto hide the name from the Prelude).
 
+
+
+
+-- num ::-= (any natural number)
+-- var ::= (Any Variable name)
+-- macro ::= (Any String name)
+
+type Num = Int
+type Var = String
+type Macro = String
+
+--Prog = element | cmd; prog	Sequence of commands
+
+type Prog = [Cmd]
+
+-- mode ::= down | up	Pen status
+
+type Mode = Down
+		  | Up
+          deriving(Show, Eq)
+		  
+
+-- expr ::= var				variable ref
+--		|	num				literal number
+--		|	expr + expr		addition expression
+
+
+type Expr = Var Var
+		  | Num Num
+		  | Add Expr Expr
+		  deriving(Eq, Show)
+
+-- cmd ::=  pen mode					Change pen mode
+--		|	move (expr, expr)			move pen to new position		  
+--		|	define macro (var*) {prog}	Define a macro
+--		|	call macro ( expr*) 		invoke a macro
+
+type Cmd = Pen Mode
+        |  Move Expr, Expr
+		|  Define Macro [Var] Prog
+		|  Call Macro [Expr]
+
+
 -- 2. Define a MiniLogo macro line (x1, y1, x2, y2) that draws a line.
+line :: Macro
+line = Define Macro [x1, y1, x2, y2] [Move x1 y1, Pen Down, Move x1 y2, Move x2 y2, Move x2 y1, Move x1 y1] 
+
 
 -- 3. use the line macro you just defined a new minilogo macro nix (x, y, w, h)
 -- this function will draw a big X from the origin (x, y) your definition
