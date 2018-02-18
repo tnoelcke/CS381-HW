461a831511433e648566508213a7c2daf375b630
Thomas Noelcke <noelcket@oregonstate.edu>
2018-02-18 08:53:19 -0800

    Finished Tests

--- HW-4/HW4/KarelSemantics.hs
+++ HW-4/HW4/KarelSemantics.hs
@@ -10,8 +10,8 @@ import KarelState
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
 test (Not t) w r = not (test t w r)
-test (Facing d) w r =  Eq d (getFacing r)
-test (Clear d) w r = undefined
+test (Facing d) w r =  d == (getFacing r)
+test (Clear d) w r =  isClear (neighbor (cardTurn d (getFacing r)) (getPos r)) w
 test (Beeper) w r = hasBeeper (getPos r)  w
 test (Empty) w r = isEmpty r
 
@@ -25,11 +25,11 @@ stmt PickBeeper _ w r = let p = getPos r
 stmt Move _ w r = undefined
 stmt PutBeeper _ w r = undefined
 stmt (Turn d) _ w r = undefined
-stmt (Call m) Defs w r = undefined
-stmt (Iterate i s) Defs w r = undefined
-stmt (If t s s) Defs w r = undefined
-stmt (While t s) Defs w r = undefined
-stmt (Block (s:xs)) Defs w r = undefined 
+stmt (Call m) d w r = undefined
+stmt (Iterate i s) d w r = undefined
+stmt (If t s1 s2) d w r = undefined
+stmt (While t s) d w r = undefined
+stmt (Block (s:xs)) d w r = undefined 
     
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
