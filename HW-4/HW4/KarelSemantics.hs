4e33790bf806d5f988e9d11593ff20d78302e0ae
Thomas Noelcke <noelcket@oregonstate.edu>
2018-02-17 17:18:00 -0800

    Started working on HW 4

--- HW-4/HW4/KarelSemantics.hs
+++ HW-4/HW4/KarelSemantics.hs
@@ -9,7 +9,11 @@ import KarelState
 
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
-test = undefined
+test (Not t) w r = not (test t w r)
+test (Facing d) w r =  Eq d (getFacing r)
+test (Clear d) w r = undefined
+test (Beeper) w r = hasBeeper (getPos r)  w
+test (Empty) w r = isEmpty r
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
@@ -18,7 +22,14 @@ stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
-stmt _ _ _ _ = undefined
+stmt Move _ w r = undefined
+stmt PutBeeper _ w r = undefined
+stmt (Turn d) _ w r = undefined
+stmt (Call m) Defs w r = undefined
+stmt (Iterate i s) Defs w r = undefined
+stmt (If t s s) Defs w r = undefined
+stmt (While t s) Defs w r = undefined
+stmt (Block (s:xs)) Defs w r = undefined 
     
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
