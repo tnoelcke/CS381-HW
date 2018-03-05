eb4f522ab7889c8d275317f40c584576e308020b
Thomas <noelcket@oregonstate.edu>
2018-03-05 07:55:10 -0800

    Worked on midterm prep

--- Exam 2 Prep/Move.hs
+++ Exam 2 Prep/Move.hs
@@ -6,3 +6,12 @@ data Move = JumpTo Pos      -- move to that given position.
           | GoUp Int        -- move vertically
           | GoRight Int     -- move horizontally
           | Seq Move Move   -- do one move followed by another.
+
+
+
+sem :: Move -> Pos -> Pos
+sem (JumpTo p) s = p
+sem (GoUp i) (x, y) = (x, y + i)
+sem (GoRight i) (x, y) = (x + i, y)
+sem (Mov r l) p = sem l (sem r p)
+
